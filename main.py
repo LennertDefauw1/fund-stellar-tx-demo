44ca8676d13412d96dd7fde71c1da7349c97e06d
LennertDefauw <lennert.defauw@jimber.org>
2022-10-25 13:26:12 +0200

    chore: add dotenv variables

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-def print_hi(name):
-    pass
+import asyncio
+
+from stellar_sdk import Asset, ServerAsync, Keypair, TransactionBuilder, Network
+from stellar_sdk.client.aiohttp_client import AiohttpClient
+
+from dotenv import dotenv_values
+
+
+config = dotenv_values(".env")
+
+address_bob = 'GBQIZ5G4OY7SIYFUWJWJEKGGMTCW5SX54GCC3TF6REDFW7P2IBGMWJ3T'
+global kp_alice
+
+
+def get_fund_keypair():
+    global kp_alice
+    kp_alice = config['SECRET_BOB']
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    get_fund_keypair()
